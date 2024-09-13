7ee60631d397fc4f3bb69def5fc03741073c841e
annav-120 <annacris1209@gmail.com>
2024-09-12 19:13:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def evento():
         cluster='us2',
         ssl=True
         )
-        pusher_client.trigger('conexion', 'evento', {'message': 'HOLA MUNDO'})
+        pusher_client.trigger('conexion', 'evento', {"txtTemperatura": 0.6, "txtHumedad": 35, "dpFechaHora": "2024-09-12 20:15})
