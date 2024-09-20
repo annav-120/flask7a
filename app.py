bf65dd494326ee09b1923ac01e51e8addd298b55
annav-120 <annacris1209@gmail.com>
2024-09-19 19:10:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,78 @@
 from flask import Flask
 from markupsafe import escape
+
 from flask import render_template
 from flask import request
+
 import pusher
+
 import mysql.connector
 import datetime
 import pytz
+
 con = mysql.connector.connect(
-host="185.232.14.52",
-database="u760464709_tst_sep",
-user="u760464709_tst_sep_usr",
-password="dJ0CIAFF="
+  host="185.232.14.52",
+  database="u760464709_tst_sep",
+  user="u760464709_tst_sep_usr",
+  password="dJ0CIAFF="
 )
+
 app = Flask(__name__)
+
 @app.route("/")
 def index():
-con.close()
-return render_template("app.html")
+    con.close()
+  
+    return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
-con.close()
-return render_template("alumnos.html")
+    con.close()
+  
+    return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-con.close()
+    con.close()
+  
+    matricula      = request.form["txtMatriculaFA"]
+    nombreapellido = request.form["txtNombreApellidoFA"]
 
-matricula = request.form["txtMatriculaFA"]
-nombreapellido = request.form["txtNombreApellidoFA"]
-
-return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
+    return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
 
 @app.route("/registrar", methods=["GET"])
 def registrar():
-args = request.args
-pusher_client = pusher.Pusher(
-app_id='1864237',
-key='fe0a6fda0635d4db01ce',
-secret='e5c4c8f921f883404989',
-cluster='us2',
-ssl=True
-)
-if not con.is_connected():
-con.reconnect()
-cursor = con.cursor()
-sql = "INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora) VALUES (%s, %s, %s)"
-val = (args["temperatura"], args["humedad"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
-cursor.execute(sql, val)
-con.commit()
-con.close()
-pusher_client.trigger("registrosTiempoReal", "registroTiempoReal", args)
-return args
+    args = request.args
+    pusher_client = pusher.Pusher(
+        app_id='1864237',
+        key='fe0a6fda0635d4db01ce',
+        secret='e5c4c8f921f883404989',
+        cluster='us2',
+        ssl=True
+    )
+
+    if not con.is_connected():
+        con.reconnect()
+    cursor = con.cursor()
+    
+    sql = "INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora) VALUES (%s, %s, %s)"
+    val = (args["temperatura"], args["humedad"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
+    cursor.execute(sql, val)
+
+    con.commit()
+    con.close()
+ 
+    pusher_client.trigger("registrosTiempoReal", "registroTiempoReal", args)
+    return args
+
 @app.route("/buscar")
 def buscar():
-if not con.is_connected():
-con.reconnect()
-cursor = con.cursor()
-cursor.execute("SELECT * FROM sensor_log ORDER BY Id_Log DESC")
-registros = cursor.fetchall()
-con.close()
-return registros
-@app.route("/contacto")
-def contacto():
-if con.is_connected():
-con.close()
-return render_template("contacto.html")
+    if not con.is_connected():
+        con.reconnect()
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM sensor_log ORDER BY Id_Log DESC")
+    registros = cursor.fetchall()
+
+    con.close()
+
+    return registros
