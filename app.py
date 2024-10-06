9332623daabb438a5d17012384d10dc1e51a1060
annav-120 <annacris1209@gmail.com>
2024-10-06 16:56:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -86,3 +86,79 @@ def contacto():
 
 if __name__ == "__main__":
     app.run(debug=True)
+@app.route("/guardar", methods=["POST"])
+def guardar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id          = request.form["id"]
+    temperatura = request.form["temperatura"]
+    humedad     = request.form["humedad"]
+    fechahora   = datetime.datetime.now(pytz.timezone("America/Matamoros"))
+    
+    cursor = con.cursor()
+
+    if id:
+        sql = """
+        UPDATE sensor_log SET
+        Temperatura = %s,
+        Humedad     = %s
+        WHERE Id_Log = %s
+        """
+        val = (temperatura, humedad, id)
+    else:
+        sql = """
+        INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora)
+                        VALUES (%s,          %s,      %s)
+        """
+        val =                  (temperatura, humedad, fechahora)
+    
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    notificarActualizacionTemperaturaHumedad()
+
+    return make_response(jsonify({}))
+
+@app.route("/editar", methods=["GET"])
+def editar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id = request.args["id"]
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT Id_Log, Temperatura, Humedad FROM sensor_log
+    WHERE Id_Log = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    registros = cursor.fetchall()
+    con.close()
+
+    return make_response(jsonify(registros))
+
+@app.route("/eliminar", methods=["POST"])
+def eliminar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id = request.form["id"]
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    DELETE FROM sensor_log
+    WHERE Id_Log = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    notificarActualizacionTemperaturaHumedad()
+
+    return make_response(jsonify({}))
